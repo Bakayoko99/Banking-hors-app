1764e14dd4f3cc6faa7aa48e1677108be7932b39
Bakagnan <cr7sendu@gmail.com>
2021-10-08 19:22:50 +0200

    grid

--- banking-app/src/components/FacturePaye.jsx
+++ banking-app/src/components/FacturePaye.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/FacturePayeStyle.css'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Checkbox from '@mui/material/Checkbox';
+import axios from 'axios';
+
+
+
 
 const Facturepaye = () => {
+
+    useEffect( async () => {
+
+        const response = await axios.get('https://test.soan-solutions.io/test_front/datas')
+
+        console.log(response);
+     
+        
+    });
+
+
     return (
         <div>
-            <h1>Facturepaye</h1>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <h3><Checkbox/>1 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>2 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>3 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3><Checkbox/>4 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>5 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>6 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3><Checkbox/>7 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>8 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>9 ok</h3>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     );
 }
--- banking-app/src/components/PasPayer.jsx
+++ banking-app/src/components/PasPayer.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import '../styles/PasPayerStyle.css'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+
 
 const Paspayer = () => {
-    
+
+ 
+
     return (
         <div>
-            <h1>Paspayer</h1>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                <Grid item xs={4}>
+                        <h3>1 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>2 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>3 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>4 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>5 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>6 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>7 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>8 ok</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3>9 ok</h3>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     );
 }
